faf5617c6269216bbd1d49d2b43a6f34ede99c93
denys <Desmond745@gmail.com>
2018-03-05 16:51:43 +0200

    Setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TodoListModule } from './todo-list/todo-list.module'
-
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from './../environments/environment';
 
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component'
+import { CoursesListComponent } from './courses-list/courses-list.component';
+
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesListComponent
   ],
   imports: [
     BrowserModule,
-    TodoListModule
+    TodoListModule,
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFireDatabaseModule,
+      AngularFireAuthModule
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+      apiKey: "AIzaSyBdasw97-AJ20khK9w34Y85Zakw0aXZAi0",
+      authDomain: "to-do-list-database.firebaseapp.com",
+      databaseURL: "https://to-do-list-database.firebaseio.com",
+      projectId: "to-do-list-database",
+      storageBucket: "to-do-list-database.appspot.com",
+      messagingSenderId: "1097738405738"
+  }
 };
